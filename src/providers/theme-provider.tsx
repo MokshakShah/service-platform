3ee27ee9ba107ba2c2d14f8e8d93008b1bd67c60
MokshakShah <mokshak18@gmail.com>
2026-01-23 01:59:19 +0530

    Update on providers

--- src/providers/theme-provider.tsx
+++ src/providers/theme-provider.tsx
@@ -2,7 +2,6 @@
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-themes/dist/types"
 import Head from "next/head"
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
